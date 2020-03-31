c75f775d54fa5d00d78ebb0a7b46680653abd193
Rom Ezer <romezer@gmail.com>
2020-04-01 00:38:48 +0300

    Added form submission

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,7 +9,29 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <br></br>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
     <Link to="/">Go back to the homepage</Link>
+    
   </Layout>
 )
 
